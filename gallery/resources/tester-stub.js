dd6d624f7c06a8bc67c57fb22830219f69b14a71
Aaron Rudkin <junk_signups@rudkin.ca>
2021-02-16 21:08:39 -0800

    Bug fix pass on the gallery -- only remaining issue is video autoplay

--- gallery/resources/tester-stub.js
+++ gallery/resources/tester-stub.js
@@ -3,7 +3,7 @@ let slide_set = INSERT_SLIDES_HERE;
 function populate() {
 	let dd_html = "";
 	for(let i=0; i!=slide_set.length; i++) {
-		dd_html += `<option value="${i}">${slide_set[i]["header"]}</option>`;
+		dd_html += `<option value="${i}">${slide_set[i]["title"]}</option>`;
 	}
 
 	document.getElementById("slide_selector").innerHTML = dd_html;
@@ -16,27 +16,36 @@ function swapSlide() {
 	const value = dropdown.options[dropdown.selectedIndex].value;
 	const slide = slide_set[value];
 
-	document.getElementById("slide_link").href = slide["link"];
+	document.getElementById("slide_link").href = `https://voteview.com${slide["link"]}`;
 	document.getElementById("slide_caption").innerHTML = slide["caption"];
 	document.getElementById("slide_header").innerHTML = slide["title"];
+	document.getElementById("prototype_title").value = slide["title"];
+	document.getElementById("prototype_caption").value = slide["caption"];
 	if(slide["video"] !== undefined) {
 		document.getElementById("slide_image").style.display = "none";
 		document.getElementById("slide_video").style.display = "inline";
-		document.getElementById("slide_video").src = slide["video"];
+		document.getElementById("slide_video").src = `../images/${slide["video"]}`;
 	} else {
 		document.getElementById("slide_image").style.display = "inline";
-		document.getElementById("slide_image").src = slide["image"];
+		document.getElementById("slide_image").src = `../images/${slide["image"]}`;
 		document.getElementById("slide_video").src = "";
 		document.getElementById("slide_video").style.display = "none";
 	}
 
 	if(slide["mask"] != undefined) {
-		document.getElementById("slide_item").className = `item item_${slide["mask"]}`;
+		document.getElementById("slide_item").className = `item active item_${slide["mask"]}`;
+
+		for(let i=0; i != document.getElementById("slide_mask").options.length; i++) {
+			if(slide["mask"] == document.getElementById("slide_mask").options[i].value) { 
+				document.getElementById("slide_mask").options[i].selected = true;
+				break;
+			}
+		}
 	} else {
-		document.getElementById("slide_item").className = "item";
+		document.getElementById("slide_item").className = "item active";
 	}
 
-	document.getElementById("slide_mask").options[0].selected = true;
+
 }
 
 function swapMask() {
@@ -44,10 +53,15 @@ function swapMask() {
 	const value = dropdown.options[dropdown.selectedIndex].value;
 
 	if(value) {
-		document.getElementById("slide_item").className = `item item_${value}`;
+		document.getElementById("slide_item").className = `item active item_${value}`;
 	} else {
-		document.getElementById("slide_item").className = "item";
+		document.getElementById("slide_item").className = "item active";
 	}
+}
+
+function updateSlide() {
+	document.getElementById("slide_header").innerHTML = document.getElementById("prototype_title").value;
+	document.getElementById("slide_caption").innerHTML = document.getElementById("prototype_caption").value;
 }
 
 populate();
